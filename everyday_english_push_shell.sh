7a529835b482d526400c54b9f59c0fecc38590fe
liueleven@aliyun.com <liueleven@aliyun.com>
2018-07-15 00:52:20 +0800

    ÿÈÕӢÓï

--- everyday_english_push_shell.sh
+++ everyday_english_push_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 'ready operating...'
+echo '-------------------------ready operating...----------------'
 git add -A
 git commit -m'√ø»’”¢”Ô'
 git push origin
-echo 'git push to origin is ok!'
+echo '-------------------------git push to origin is ok!---------'
