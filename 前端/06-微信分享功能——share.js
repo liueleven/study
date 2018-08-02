0368cb5e1fa04814826cae8d7666b0188d760c72
liueleven@aliyun.com <liueleven@aliyun.com>
2018-08-02 10:59:29 +0800

    ¸üÐÂ

--- 前端/06-微信分享功能——share.js
+++ 前端/06-微信分享功能——share.js
@@ -1,18 +1,14 @@
-var baseUrl = 'http://www.liuzuoqi.com'; // 后台api TODO 需要替换
-// var baseUrl = 'http://meg-test.sunnyxiao.com'; // 后台api
+var baseUrl = 'http://www.example.com'; // 后台api 
 var sharedUrl;
-var shareTitle = 'MGE电竞';
-var shareDesc = 'MGE——Make Great Efforts 立志为电竞玩家做出最大的努力，做玩家最贴心的电竞服务圈';
+var shareTitle = '自定义标题';
+var shareDesc = '自定义描述';
 function getSignUrl() {
-    // debugger
-    // alert("getSignUrl")
     sharedUrl = location.href.split('#')[0];  //获取当前页面的url
     sharedUrl = encodeURIComponent(sharedUrl);
     return baseUrl + '/verity/wechat/share?url=' + sharedUrl;
 }
 var config = {
     title: shareTitle, // 分享标题
-    // title: 'MGE电竞', // 分享标题
     desc: shareDesc,
     imgUrl: 'http://wx4.sinaimg.cn/mw690/0060lm7Tly1ftv42xif9uj30hs0hs3z3.jpg', // 分享图标
     link: sharedUrl, // 分享链接，该链接域名必须与当前企业的可信域名一致
@@ -39,7 +35,6 @@ function doConfig(result) {
             'onMenuShareTimeline'
         ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
-
     wx.ready(function () {
         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
         // 分享到朋友圈
@@ -56,8 +51,6 @@ function doConfig(result) {
     //     }
     // });
 }
-
-
 $.get(getSignUrl(), function (result, status) {
     if (status === 'success') {
         doConfig(result);
@@ -65,4 +58,3 @@ $.get(getSignUrl(), function (result, status) {
         alert('无法获取token信息');
     }
 });
-
